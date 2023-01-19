8007d3932ff98bb5974d170ea3f29941f70c7e8f
ABIN_K_THANKACHAN <abinkthankachan5@gmail.com>
2023-01-19 22:15:31 +0530

    all

---
+++ django-deployment of model/home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from home import views
+urlpatterns = [
+    path('',views.index),
+    path('about',views.about,name="about"),
+    path('contact/',views.contact,name="contact"),
+    path('popularity/',views.popularity,name="popularity"),
+    path('recommendation/',views.recommendation,name="recommendation"),
+    path('rec_page/',views.rec_page,name='rec_page'),
+    path('nonremix_page/',views.nonremix_page,name='nonremix_page'),
+    path('result_page/',views.result_page,name='result_page')
+
+]
